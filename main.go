4e134d5f31a8cc4538bebcf161d3115c8c1ffc19
AshKmo <ashkmo33@gmail.com>
2024-07-30 16:32:20 +1000

    http server working

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"log"
+)
+
+func main() {
+	addr := "192.168.0.26:8080"
+
+	mux := http.NewServeMux()
+
+	httpServer := &http.Server{
+		Addr: addr,
+		Handler: mux,
+	}
+
+	mux.Handle("/", http.FileServer(http.Dir("site")))
+
+	mux.HandleFunc("/api/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hi"))
+	})
+
+	fmt.Println("Server started at " + addr)
+
+	log.Fatal(httpServer.ListenAndServe())
+}
